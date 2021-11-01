564895aed1ed877571af9027028056501f245225
Sobulachi Chinda <sobychinda@gmail.com>
2021-11-01 06:19:21 +0000

    Created frontend app with react
    configured backend app to provide token for frontend app to make calls from it and then created frontend app

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='api-home'),
     path('tweet-list/', views.tweetList, name='tweet-list'),
     path('tweet-detail/<str:pk>/', views.tweetDetail, name='tweet-detail'),
-    path('tweet-create/', views.tweetCreate, name='tweet-create')
+    path('tweet-create/', views.tweetCreate, name='tweet-create'),
+    path('tweet-like/<str:pk>/', views.tweetLike, name='tweet-like'),
+    path('tweet-delete/<str:pk>/', views.tweetDelete, name='tweet-delete')
 ]
